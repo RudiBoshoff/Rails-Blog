f80a047b2a32000d9d9204b59adaf9b417fa3834
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-07-07 16:02:26 +0200

    add styling with bulma framework

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,45 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
-
+    @post = Post.new
   end
-  
-  def create
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+    
+    
+  def update
+  @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      flash[:success] = "Object was successfully updated"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
