e2b786921889724ed8cbd221b0cb23de03111e05
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-07-16 16:41:32 +0200

    finishes CRD for comments

---
+++ db/migrate/20200715085942_add_posts_to_comments.rb
@@ -0,0 +1,5 @@
+class AddPostsToComments < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :comments, :post, null: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_091418) do
+ActiveRecord::Schema.define(version: 2020_07_15_085942) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_07_06_091418) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
